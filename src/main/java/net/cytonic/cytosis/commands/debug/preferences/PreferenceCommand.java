ee234b3df3024bc946bbaff08f4249a9f52f6498
Webhead1104 <webhead1104@hotmail.com>
2026-08-23 19:06:37 -0500

    fix: /preference had no permission check, letting any player self-grant fly/vanish
    PreferenceCommand and its get/set subcommands never called
    setCondition(...), unlike every other staff/admin command. Since
    subcommand-path conditions in this graph chain with their parent's,
    gating the parent with IS_ADMIN (matching CooldownCommand/
    ParticleCommand's convention) is sufficient to cover /preference get
    and /preference set as well.

--- src/main/java/net/cytonic/cytosis/commands/debug/preferences/PreferenceCommand.java
+++ src/main/java/net/cytonic/cytosis/commands/debug/preferences/PreferenceCommand.java
@@ -1,5 +1,6 @@
 package net.cytonic.cytosis.commands.debug.preferences;
 
+import net.cytonic.cytosis.commands.utils.CommandUtils;
 import net.cytonic.cytosis.commands.utils.CytosisCommand;
 import net.cytonic.cytosis.utils.Msg;
 
@@ -7,6 +8,7 @@ public class PreferenceCommand extends CytosisCommand {
 
     public PreferenceCommand() {
         super("preference", "pref");
+        setCondition(CommandUtils.IS_ADMIN);
         setDefaultExecutor((sender, _) -> sender.sendMessage(Msg.red("Please specify an operation!")));
     }
 }
